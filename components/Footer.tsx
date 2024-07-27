a8e852a2536845816ca1ac07ef083db64f16af5d
Amar kumar <aknbo30@gmail.com>
2024-07-27 18:08:12 +0530

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,6 +9,10 @@ import {
   FaMailBulk,
   FaTwitter,
   FaYoutube,
+  FaMapMarkerAlt,
+  FaPhoneAlt,
+  FaEnvelope,
+  FaMapSigns,
 } from "react-icons/fa";
 
 import { FaXTwitter } from "react-icons/fa6";
@@ -26,57 +30,50 @@ const Footer = () => {
   return (
     <footer className="mt-24 pt-5 lg:pt-8 pb-2 lg:pb-8 bg-black bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20">
       <div className="flex flex-col">
-        <div className="lg:hidden flex justify-center mb-7 font-bold"/>
-      
+        <div className="lg:hidden flex justify-center mb-7 font-bold" />
+
         <div className="flex justify-center text-sm lg:text-3xl space-x-4 mb-9">
           <a href="https://discord.com/invite/Hz5UxnUNBV" target="_black">
             <span className="sr-only">Discord</span>
-           <FaDiscord/>
+            <FaDiscord />
           </a>
-          <div className=" border-r-2 transform rotate-12 "  />
+          <div className=" border-r-2 transform rotate-12 " />
           <a href="https://x.com/dsu_devhack24" target="_black">
             <span className="sr-only">Twitter</span>
-            <FaXTwitter/>
-
+            <FaXTwitter />
           </a>
           <div className=" border-r-2 transform rotate-12" />
 
           <a href="https://www.linkedin.com/in/dsu-devhack/" target="_black">
             <span className="sr-only ">LinkedIn</span>
-            <FaLinkedinIn/>
-
+            <FaLinkedinIn />
           </a>
           <div className=" border-r-2 transform rotate-12" />
 
           <a href="https://www.youtube.com/@DSUDevHack" target="_black">
             <span className="sr-only">YouTube</span>
-            <FaYoutube/>
+            <FaYoutube />
           </a>
           <div className=" border-r-2 transform rotate-12" />
 
           <a href="https://www.instagram.com/dsudevhack/" target="_black">
             <span className="sr-only">Instagram</span>
-            <FaInstagram/>
+            <FaInstagram />
+          </a>
+          <div className="border-r-2 transform rotate-12" />
+          <a href="https://maps.app.goo.gl/UBETR1E3qbbqNDtY8" target="_blank">
+            <span className="sr-only">Location</span>
+            <FaMapMarkerAlt />
           </a>
         </div>
-        <div className="lg:hidden flex justify-center mb-7 font-bold"/>
-      
-        <div className="flex justify-center text-sm space-x-4 mb-0">
-        <a href="mailto:dsudevhack@dsu.edu.in">
-              Email
-            </a>
-            <a className="pl-4" href="https://discord.com/invite/Hz5UxnUNBV" target="_blank" rel="noreferrer">
-              Discord
-            </a>
-            <a className="pl-4" href="https://maps.app.goo.gl/UBETR1E3qbbqNDtY8" target="_blank" rel="noreferrer">
-              Venue
-            </a>
-            <a className="pl-4" href="tel:8154852286">
-              Phone
-            </a>
-        </div>
-      
+        <div className="lg:hidden flex justify-center mb-7 font-bold" />
 
+        <div className="flex justify-center text-sm space-x-4 mb-0">
+          <a href="mailto:dsudevhack@dsu.edu.in">Email</a>
+          <a className="pl-4" href="tel:8154852286">
+            Call
+          </a>
+        </div>
       </div>
     </footer>
   );
