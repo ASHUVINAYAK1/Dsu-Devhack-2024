5d92424052645763dc2a9729cda73a09ad4f0208
Ganesh Shinde <sindheganesh2004@gmail.com>
2024-07-31 15:50:24 +0530

    Added button and discord link

--- components/HeroSection/Content.tsx
+++ components/HeroSection/Content.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { motion, useAnimation, useInView } from "framer-motion";
 import CountdownTimer from "./CountdownTimer";
 import "../../styles/hero.css";
+import { FaDiscord } from "react-icons/fa";
 
 const Content: React.FC = () => {
   useEffect(() => {
@@ -60,14 +61,16 @@ const Content: React.FC = () => {
           <motion.div
             variants={childVariants}
             animate="visible"
-            className="text-white w-[490px] font-poppins m-auto md:mt-4 mt-2 mb-5 md:text-[1.1rem] text-[1.1rem] sm:text-lg"
+            className="text-white w-[490px] font-poppins m-auto md:mt-4 mt-2 mb-5 md:text-[1.1rem] text-[1.1rem] sm:text-lg leading-tight"
           >
-            <span className="text-purple-500 w-[490px] font-poppins font-bold m-auto md:mt-4 mt-2 mb-5 md:text-[1.2rem] text-[1.2rem] sm:text-lg">Dayananda Sagar University</span>
+            <span className="text-purple-500 w-[490px] font-poppins font-bold m-auto md:mt-4 mt-2 mb-5 md:text-[1.2rem] text-[1.2rem] sm:text-lg leading-3">
+              Dayananda Sagar University
+            </span>
             <br />
             Department of Computer Science & Eng
-            <span className="hidden md:inline font-poppins">ineering</span>
+            <span className="hidden md:inline font-poppins leading-3">ineering</span>
             <br />
-            <span className="text-xs">Harohalli, Bengaluru</span>
+            <span className="text-xs leading-tight">Harohalli, Bengaluru</span>
           </motion.div>
 
           <motion.div
@@ -76,6 +79,15 @@ const Content: React.FC = () => {
           >
             DevHack
           </motion.div>
+          <a
+            href="https://discord.com/invite/Hz5UxnUNBV"
+            target="_blank"
+            className="inline-flex flex-row items-center bg-blue-600 text-white px-3 py-[2px] rounded gap-1"
+          >
+            <FaDiscord />
+            <span className="text-white">Discord</span>
+          </a>
+
           <motion.div
             variants={childVariants}
             className="md:text-1xl text-[1rem] text-purple-500 font-content font-bold m-1 mb-2"
@@ -89,17 +101,24 @@ const Content: React.FC = () => {
           animate="visible"
           className=" text-white max-w-[360px] font-poppins m-auto md:mt-1 md:text-[1.1rem]"
         >
-          Compete to Win Big: <span className="orange-logo md:mt-1 md:text-[1.1rem] font-poppins">₹2 Lakh Cash Prize.</span>
-          <br /><span className="orange-logo md:mt-1 md:text-[1.1rem] font-poppins">3 Lakh+ Pool Prize </span>Awaits!
+          Compete to Win Big:{" "}
+          <span className="orange-logo md:mt-1 md:text-[1.1rem] font-poppins">
+            ₹2 Lakh Cash Prize.
+          </span>
+          <br />
+          <span className="orange-logo md:mt-1 md:text-[1.1rem] font-poppins">
+            3 Lakh+ Pool Prize{" "}
+          </span>
+          Awaits!
         </motion.p>
       </div>
       <motion.div className="text-md text-white">
         <CountdownTimer />
-        {/* <button
+        <button
           className={`m-1 ${" mt-6 rounded-xl bg-gradient-to-l from-purple-500 to-pink-500 text-white"} w-full py-2 rounded-md font-semibold transition-colors duration-300`}
         >
           Register Now!!
-        </button> */}
+        </button>
       </motion.div>
       <div className="">
         <div
